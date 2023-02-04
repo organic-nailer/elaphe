0cef1ad90a179b3f4fb48ce315d7c06b2b93b459
organic-nailer <hykwyuk0125@gmail.com>
2023-02-04 15:09:13 +0900

    split lib crate

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,6 @@
 use std::env;
-use std::fs;
-use std::fs::{File, OpenOptions};
-use std::io::Write;
-use std::path::Path;
 use std::process::Command;
 use std::str;
-use std::time::SystemTime;
-
-mod bytecode;
-mod bytecompiler;
-mod parser;
-mod pyobject;
-
-use crate::bytecode::OpCode;
-use crate::bytecompiler::ByteCompiler;
-use crate::pyobject::PyObject;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -23,34 +9,8 @@ fn main() {
         return;
     }
     let source = &args[1];
-    let node = parser::parse(source);
-    if node.is_none() {
-        println!("failed to parse the passed source: {}", source);
-        return;
-    }
-    let node = node.unwrap();
-    let compiler = ByteCompiler::run(&node, source);
-
-    let operation_list = compiler.byte_operations;
-    let constant_list = compiler.constant_list;
-    let name_list = compiler.name_list;
-
     let output = "main.pyc";
-    {
-        let path = Path::new(output);
-        match fs::remove_file(path) {
-            Result::Ok(_) => (), // println!("file removed"),
-            Result::Err(_) => println!("file does not exists"),
-        }
-        let mut file = OpenOptions::new()
-            .create(true)
-            .write(true)
-            .open(&path)
-            .unwrap();
-
-        write_header(&mut file);
-        write_py_code(&mut file, &operation_list, &constant_list, &name_list);
-    }
+    elaphe::run(output, source);
 
     match Command::new("bash")
         .args(&["-c", "python main.pyc"])
@@ -58,130 +18,5 @@ fn main() {
     {
         Ok(e) => println!("{}", str::from_utf8(&e.stdout).unwrap()),
         Err(e) => println!("Error: {}", e),
-    }
-}
-
-fn write_header(file: &mut File) {
-    file.write(&[0x61, 0x0D, 0x0D, 0x0A]).unwrap(); // Magic Number
-    file.write(&[0x00, 0x00, 0x00, 0x00]).unwrap(); // Flag(PEP552)
-
-    let now = SystemTime::now()
-        .duration_since(SystemTime::UNIX_EPOCH)
-        .unwrap()
-        .as_secs() as u32;
-    file.write(&(now.to_le_bytes())).unwrap(); // Timestamp
-    let file_size: u32 = 0;
-    file.write(&(file_size.to_le_bytes())).unwrap();
-}
-
-fn write_py_code(
-    file: &mut File,
-    operation_list: &[OpCode],
-    constant_list: &[PyObject],
-    name_list: &[PyObject],
-) {
-    file.write(&[0xE3]).unwrap(); // ObjectType
-    file.write(&(0u32.to_le_bytes())).unwrap(); // ArgCount
-    file.write(&(0u32.to_le_bytes())).unwrap(); // PosOnlyArgCount
-    file.write(&(0u32.to_le_bytes())).unwrap(); // KwOnlyArgCount
-    file.write(&(0u32.to_le_bytes())).unwrap(); // NumLocals
-    let stack_size = bytecode::calc_stack_size(&operation_list);
-    if stack_size < 0 {
-        panic!("invalid stack size: {}", stack_size);
-    }
-    file.write(&(stack_size as u32).to_le_bytes()).unwrap(); // StackSize
-    file.write(&(64u32.to_le_bytes())).unwrap(); // Flags
-
-    let codes = bytecode::compile_code(&operation_list);
-    write_py_string(file, &codes, false);
-
-    // 定数一覧
-    write_py_small_tuple(file, &constant_list);
-
-    // 名前一覧
-    write_py_small_tuple(file, &name_list);
-
-    {
-        // SmallTuple: ローカル変数一覧
-        let tuple_len = 0u8;
-        file.write(&[0xA9]).unwrap();
-        file.write(&[tuple_len]).unwrap();
-    }
-    {
-        // ObjectRef: 自由変数
-        // file.write(&[0x72]).unwrap();
-        // let target = 3u32;
-        // file.write(&(target.to_le_bytes())).unwrap();
-        let tuple_len = 0u8;
-        file.write(&[0xA9]).unwrap();
-        file.write(&[tuple_len]).unwrap();
-    }
-    {
-        // ObjectRef: セル変数
-        // file.write(&[0x72]).unwrap();
-        // let target = 3u32;
-        // file.write(&(target.to_le_bytes())).unwrap();
-        let tuple_len = 0u8;
-        file.write(&[0xA9]).unwrap();
-        file.write(&[tuple_len]).unwrap();
-    }
-    // ファイル名
-    write_py_short_ascii(file, "main.py".as_bytes());
-    // 名前
-    write_py_short_ascii_interned(file, "<module>".as_bytes(), true);
-    // first line
-    file.write(&(1u32.to_le_bytes())).unwrap();
-    // line table
-    {
-        file.write(&[0xF3]).unwrap();
-        file.write(&(0u32.to_le_bytes())).unwrap();
-    }
-}
-
-fn write_py_string(file: &mut File, value: &[u8], register_ref: bool) {
-    let object_type = 0x73u8 | ((register_ref as u8) << 7);
-    file.write(&[object_type]).unwrap(); // ObjectType
-    let str_len = value.len() as u32;
-    file.write(&(str_len.to_le_bytes())).unwrap();
-    file.write(value).unwrap();
-}
-
-fn write_py_int(file: &mut File, value: i32, register_ref: bool) {
-    let object_type = 0x69u8 | ((register_ref as u8) << 7);
-    file.write(&[object_type]).unwrap(); // ObjectType
-    file.write(&(value.to_le_bytes())).unwrap();
-}
-
-fn write_py_none(file: &mut File, register_ref: bool) {
-    let object_type = 0x4Eu8 | ((register_ref as u8) << 7);
-    file.write(&[object_type]).unwrap();
-}
-
-fn write_py_short_ascii_interned(file: &mut File, value: &[u8], register_ref: bool) {
-    let object_type = 0x5Au8 | ((register_ref as u8) << 7);
-    file.write(&[object_type]).unwrap();
-    let str_len = value.len() as u8;
-    file.write(&[str_len]).unwrap();
-    file.write(value).unwrap();
-}
-
-fn write_py_short_ascii(file: &mut File, value: &[u8]) {
-    file.write(&[0xFA]).unwrap();
-    let str_len = value.len() as u8;
-    file.write(&[str_len]).unwrap();
-    file.write(value).unwrap();
-}
-
-fn write_py_small_tuple(file: &mut File, value_list: &[PyObject]) {
-    let tuple_len = value_list.len() as u8;
-    file.write(&[0x29]).unwrap();
-    file.write(&[tuple_len]).unwrap();
-    for c in value_list {
-        match *c {
-            PyObject::Int(v, r) => write_py_int(file, v, r),
-            PyObject::Str(v, r) => write_py_string(file, v.as_bytes(), r),
-            PyObject::Ascii(v, r) => write_py_short_ascii_interned(file, v.as_bytes(), r),
-            PyObject::None(r) => write_py_none(file, r),
-        }
     }
 }
